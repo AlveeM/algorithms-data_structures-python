071776dae7a23dfcd45ac499ee7f6f40fae258cc
Alvee Akand <alvee.akand@outlook.com>
2020-05-04 23:41:42 -0400

    added list permutations

---
+++ algorithms/recursion/list-permutations.py
@@ -0,0 +1,61 @@
+import copy
+
+def permute(l):
+    """
+    Return a list of permutations
+
+    Examples:
+       permute([0, 1]) returns [ [0, 1], [1, 0] ]
+
+    Args:
+      l(list): list of items to be permuted
+
+    Returns:
+      list of permutation with each permuted item being represented by a list
+    """
+    output = []
+    _permute_recurse(l, 0, output)
+    return output
+
+def _permute_recurse(arr, start, result):
+    if start >= len(arr):
+        result.append(arr.copy())
+    else:
+        for i in range(start, len(arr)):
+            arr[start], arr[i] = arr[i], arr[start]
+            _permute_recurse(arr, start + 1, result)
+            arr[start], arr[i] = arr[i], arr[start]
+
+
+
+# Test Cases
+
+# Helper Function
+def check_output(output, expected_output):
+    """
+    Return True if output and expected_output
+    contains the same lists, False otherwise.
+
+    Note that the ordering of the list is not important.
+
+    Examples:
+        check_output([ [0, 1], [1, 0] ] ], [ [1, 0], [0, 1] ]) returns True
+
+    Args:
+        output(list): list of list
+        expected_output(list): list of list
+
+    Returns:
+        bool
+    """
+    o = copy.deepcopy(output)  # so that we don't mutate input
+    e = copy.deepcopy(expected_output)  # so that we don't mutate input
+
+    o.sort()
+    e.sort()
+    return o == e
+
+print ("Pass" if  (check_output(permute([]), [[]])) else "Fail")
+print ("Pass" if  (check_output(permute([0]), [[0]])) else "Fail")
+print ("Pass" if  (check_output(permute([0, 1]), [[0, 1], [1, 0]])) else "Fail")
+print ("Pass" if  (check_output(permute([0, 1, 2]), [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]])) else "Fail")
